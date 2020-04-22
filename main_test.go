9c1ea69b5d2cf53bc04b7098f4c2c58af6c1eb76
Hiago Balbino <hiagobalbinoo@gmail.com>
2020-04-22 19:11:25 -0300

    Add test to main file

---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/fsantiag/track-progress/src/controller"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(t *testing.T) {
+	testTable := []struct {
+		Method      string
+		HandlerFunc http.HandlerFunc
+	}{
+		{"GET", controller.Health},
+	}
+
+	for _, testData := range testTable {
+		httpTest := httptest.NewServer(http.HandlerFunc(testData.HandlerFunc))
+		defer httpTest.Close()
+
+		client := &http.Client{}
+		request, _ := http.NewRequest(testData.Method, httpTest.URL, nil)
+		response, err := client.Do(request)
+		assert.Nil(t, err, "Error performing request.")
+		defer response.Body.Close()
+
+		responseBody, _ := ioutil.ReadAll(response.Body)
+		assert.NotEmpty(t, responseBody)
+		assert.Equal(t, http.StatusOK, response.StatusCode)
+	}
+}
--- src/controller/health.go
+++ src/controller/health.go
@@ -7,5 +7,6 @@ import (
 
 // Health returns if the service is up or down
 func Health(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, "Up!")
+	response.WriteHeader(http.StatusOK)
+	fmt.Fprintf(response, "UP!")
 }
