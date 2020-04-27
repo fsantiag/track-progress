d8491d5f39a691237265b5c9cf5e5780ad84cf4b
Hiago Balbino <hiagobalbinoo@gmail.com>
2020-04-27 11:25:38 -0300

    Add configuration credentials aws

--- src/queue/configuration.go
+++ src/queue/configuration.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
@@ -15,6 +16,11 @@ func NewSession() *sqs.SQS {
 	session := session.Must(session.NewSession(&aws.Config{
 		Region:   aws.String(endpoints.UsWest2RegionID),
 		Endpoint: aws.String(util.Getenv("SQS_HOST", "http://localhost:4576")),
+		Credentials: credentials.NewStaticCredentials(
+			util.Getenv("SQS_ID", "id"),
+			util.Getenv("SQS_SECRET", "secret"),
+			util.Getenv("SQS_TOKEN", "token"),
+		),
 	}))
 
 	return sqs.New(session)
